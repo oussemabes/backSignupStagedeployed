0baba623f865f16f087b98226be5a8786c64fe1b
maahdi boudaouara <mboudaouara@mercarue.com>
2023-04-22 20:55:53 +0100

    1223

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-const { app } = require('./app');
-const http = require('http');
-require('dotenv').config()
+const { app } = require("./app");
+const http = require("http");
+require("dotenv").config();
 const server = http.createServer(app);
-const db=require('./config')
-db.connect(function(err) {
+const db = require("./config");
+db.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
 });
 server.listen(
-    process.env.USER_PORT ,
+  process.env.USER_PORT,
+  process.env.USER_SERVER,
   console.log(`server is running at port http://${process.env.USER_SERVER}`)
 );
